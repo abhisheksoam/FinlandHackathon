d66feb348ebf1600701b58e717af3ed01dea181d
Nitin <nitin070895@gmail.com>
2017-06-10 19:31:49 +0530

    first

---
+++ app/src/main/java/example/com/hospitalapp/LoginActivity.java
@@ -0,0 +1,17 @@
+package example.com.hospitalapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nitin Khurana on 10-Jun-17.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}

97b44794837848e48c8f1672c43e3b380d243cd6
Nitin Khurana <nitin070895@gmail.com>
2017-06-10 19:35:41 +0530

    "First"

--- app/src/main/java/example/com/hospitalapp/LoginActivity.java
+++ app/src/main/java/example/com/hospitalapp/LoginActivity.java
@@ -3,6 +3,7 @@ package example.com.hospitalapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by Nitin Khurana on 10-Jun-17.
@@ -13,5 +14,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        Log.e("a","a");
     }
 }
